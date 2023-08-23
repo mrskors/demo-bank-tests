2682c801d52e56554b5d341ee531242564f54ce2
Michał Skowron <mrskors@gmail.com>
2023-08-23 20:53:24 +0200

    added before to pulpit

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -3,23 +3,22 @@ import { test, expect } from '@playwright/test';
 test.describe('Pulpit tests', () => {
   test.beforeEach(async ({ page }) => {
     const url = 'https://demo-bank.vercel.app/';
-    await page.goto(url);
-  });
-  test('quick payment with correct data', async ({ page }) => {
-    // Arrange
     const userId = 'testerLO';
     const userPassword = 'password';
 
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
+  });
+  test('quick payment with correct data', async ({ page }) => {
+    // Arrange
     const reciverId = '2';
     const transferAmount = '150';
     const transferTitle = 'pizza';
     const expectedTransferReceiver = 'Chuck Demobankowy';
 
     // Act
-    await page.getByTestId('login-input').fill(userId);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
-
     await page.locator('#widget_1_transfer_receiver').selectOption(reciverId);
     await page.locator('#widget_1_transfer_amount').fill(transferAmount);
     await page.locator('#widget_1_transfer_title').fill(transferTitle);
@@ -35,17 +34,11 @@ test.describe('Pulpit tests', () => {
 
   test('successful mobile top-up', async ({ page }) => {
     // Arrange
-    const userId = 'testerLO';
-    const userPassword = 'password';
     const phoneNumber = '500 xxx xxx';
     const topupAmount = '10';
     const expectedMessage = `Doładowanie wykonane! ${topupAmount},00PLN na numer ${phoneNumber}`;
 
     //Act
-    await page.getByTestId('login-input').fill(userId);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
-
     await page.locator('#widget_1_topup_receiver').selectOption(phoneNumber);
     await page.locator('#widget_1_topup_amount').fill(topupAmount);
     await page.locator('#uniform-widget_1_topup_agreement span').click();
